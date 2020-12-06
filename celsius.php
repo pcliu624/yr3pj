078d8d5143e46c431855ca8a2d2cc1faf867fb68
pcliu624 <38576572+pcliu624@users.noreply.github.com>
2020-12-06 17:02:39 +0800

    Update celsius.php

--- celsius.php
+++ celsius.php
@@ -20,7 +20,7 @@ if ($dbhandle->connect_error) {
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="refresh" content="300">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="web.css" rel="stylesheet">
+<link href="web.css?version=$lt:?php echo time(); ?&gt;" rel="stylesheet">
   <script src="http://static.fusioncharts.com/code/latest/fusioncharts.js"></script>
   <script src="http://static.fusioncharts.com/code/latest/fusioncharts.charts.js"></script>
   <script src="http://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.ocean.js"></script>
